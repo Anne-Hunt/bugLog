2c9495c43699c42e7a90be689239f61e81701d5c
Jarrett Cameron <native@hotmail.com>
2024-04-11 13:46:13 -0600

    Created models

--- src/models/TrackedBug.js
+++ src/models/TrackedBug.js
@@ -1,5 +1,20 @@
 import { Schema } from "mongoose";
 
 export const TrackedBugSchema = new Schema({
-
+    accountId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+    bugId: { type: Schema.ObjectId, required: true, ref: 'Bug' }
 }, { toJSON: { virtuals: true } })
+
+TrackedBugSchema.virtual('tracker', {
+    localField: 'accountId',
+    foreignField: '_id',
+    ref: 'Account',
+    justOne: true
+})
+
+TrackedBugSchema.virtual('bug', {
+    localField: 'bugId',
+    foreignField: '_id',
+    ref: 'Bug',
+    justOne: true
+})
